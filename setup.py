dabc5e6d64f55734ba227d291fb7472e686b5c34
Ari Purnama <ari.purnama838@gmail.com>
2020-10-08 17:41:47 +0800

    change package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='def',
+    name='directional_feature',
     version='0.1.0',    
     description='A Algorithm for image feature extraction',
     url='https://github.com/ariaji25/def',
