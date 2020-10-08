898955e81df67e5a6aafb0399be164741246169f
Ari Purnama <ari.purnama838@gmail.com>
2020-10-08 17:37:56 +0800

    update setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='def',
+    version='0.1.0',    
+    description='A Algorithm for image feature extraction',
+    url='https://github.com/ariaji25/def',
+    author='Ari Purnama Aji',
+    author_email='ari.purnama838@gmail.com',
+    packages=['def'],
+    install_requires=['opencv-python',
+                      'numpy',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
